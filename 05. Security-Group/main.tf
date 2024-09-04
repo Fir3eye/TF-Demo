8fdd0390f30d0a5d6034bc675c297b14bee14dbb
fir3 <fir3@gmail.com>
2024-09-04 18:15:00 +0530

    security-group

---
+++ 05. Security-Group/main.tf
@@ -0,0 +1,57 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "5.65.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "ap-south-1"
+}
+
+
+resource "aws_security_group" "allow_tls" {
+  name        = "my-sg"
+  description = "Allow TLS inbound traffic"
+  vpc_id      = "vpc-0271007e93cae0e0d"
+
+  ingress {
+    description = "TLS from VPC"
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  ingress {
+    description = "TLS from VPC"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "my-new-sec"
+  }
+}
+
+
+resource "aws_instance" "sg_instance" {
+    ami = "ami-0522ab6e1ddcc7055"
+    instance_type = "t2.micro"
+}
+
+
+resource "aws_network_interface_sg_attachment" "sg_attachment" {
+  security_group_id    = aws_security_group.allow_tls.id
+  network_interface_id = aws_instance.sg_instance.primary_network_interface_id
+}
